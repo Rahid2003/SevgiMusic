5189dd0ac4957be5d76a5fc5e0a702ccfa74e4c3
Rahid2003 <104177696+Rahid2003@users.noreply.github.com>
2022-12-21 15:17:10 +0400

    update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ load_dotenv()
 que = {}
 admins = {}
 
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "Rahid_MP3") 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "Rahid_Music_Bot")
+SESSION_NAME = getenv("SESSION_NAME", "AgC0uWq2nnrxHaHmv97dhbzqC6eyx9TLVL-uiZZitcgLrBkFXgrrbgf25raPVURHDEo945qQSZPBIoHgPgbLVi1e1N0o9bIdRnGbhdmD0j3vg4UA_NCLycnE9HOB9Z8WMOI9Y8w-oMqhuO0qDRU6k9SzFWbWgHkKZWDCn1SITHIzGDOdyVnp6y3NYtU9EazImSUUYYg06gPv5zJ-jU7sI2yiOELHlM7XYSWT0rcxU-BmxTTM9377tsUU7FNn0x1Z4BrmjBAhi5E0SStmmqfUPkYflItN9Q0vV0qxlf60ywcOJjobh44tMShPccqi2q15_kMx9ZL5-M3_C5pG1sTQchLsAAAAAU87jeQA")
+BOT_TOKEN = getenv("BOT_TOKEN", "5461712721:AAFHX821IrwbRa0EA8bpezz08ASMiifGFlQ")
+BOT_NAME = getenv("BOT_NAME", "𝑴𝑺 ~ 𝑴𝒖𝒔𝒊𝒄") 
+API_ID = int(getenv("API_ID", "19485442"))
+API_HASH = getenv("API_HASH", "a03fcb372b3ec4e406b5d52f84b02e53")
+BOT_USERNAME = getenv("BOT_USERNAME", "Sevgi_Music_Bot")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5451034037").split()))
